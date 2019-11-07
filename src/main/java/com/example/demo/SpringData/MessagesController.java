42c98a62bf55af93ae146188071254b083f7baa8
hiroyuki0210 <h.tamaki@atomitech.jp>
2019-11-07 18:06:24 +0900

    ディレクトリ構造変更

--- src/main/java/com/example/demo/SpringData/MessagesController.java
+++ src/main/java/com/example/demo/SpringData/MessagesController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.SpringData;
 
 import java.util.List;
 
--- src/main/java/com/example/demo/SpringMVC/HelloController.java
+++ src/main/java/com/example/demo/SpringMVC/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.SpringMVC;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
